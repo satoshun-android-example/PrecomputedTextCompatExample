a64aed351abdfa196a5d670156bc36edfe20a1d0
SatoShun <shun.sato1@gmail.com>
2018-08-02 10:08:32 +0000

    use Futrue

--- app/src/main/java/com/github/satoshun/precomputedtextcompat/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/precomputedtextcompat/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.satoshun.precomputedtextcompat.databinding.MainActBinding
 import com.github.satoshun.precomputedtextcompat.databinding.MainItemBinding
-import java.util.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,27 +29,45 @@ class MainActivity : AppCompatActivity() {
 private class Adapter : RecyclerView.Adapter<ViewHolder>() {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
       ViewHolder(
-          MainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-          generateText()
+          MainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
       )
 
   override fun getItemCount(): Int = 1000
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    holder.binding.text.text = holder.text
+    val text = texts[position]
+    val important = predicates[position]
+
+    // new way
+    holder.binding.title.textSize = if (important) 16f else 12f
+    (holder.binding.title as AppCompatTextView).setTextFuture(
+        PrecomputedTextCompat.getTextFuture(
+            text,
+            TextViewCompat.getTextMetricsParams(holder.binding.title),
+            null
+        )
+    )
+
+    // normal way
+//    holder.binding.title.text = text
+  }
+
+  override fun getItemViewType(position: Int): Int {
+    return position
   }
 }
 
 private class ViewHolder(
-    val binding: MainItemBinding,
-    val text: String
+    val binding: MainItemBinding
 ) : RecyclerView.ViewHolder(binding.root)
 
 val random = Random()
 val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+val texts = (0..1000).map { generateText() }
+val predicates = (0..1000).map { random.nextInt(5) % 5 == 0 }
 
 private fun generateText(): String {
-  return (0..500)
+  return (0..700)
       .map { alphabet[random.nextInt(26)] }
       .joinToString("")
 }
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,12 +2,11 @@ object Vers {
   val compile_sdk = 28
   val min_sdk = 28
   val target_sdk = 28
-  val agp = "3.3.0-alpha03"
+  val agp = "3.2.0-beta05"
 //  val agp = "3.1.2"
 
-  val kotlin = "1.2.51"
-  val couroutine = "0.22.5"
-  val support_lib = "28.0.0-alpha1"
+  val kotlin = "1.2.60"
+  val couroutine = "0.24.0"
   val ktlint = "0.21.0"
 }
 
@@ -22,22 +21,13 @@ object Libs {
 
   val ktx = "androidx.core:core-ktx:1.0.0-alpha1"
 
-  val design = "com.android.support:design:${Vers.support_lib}"
   val appcompat = "androidx.appcompat:appcompat:1.0.0-beta01"
-  val support_annotations = "com.android.support:support-annotations:${Vers.support_lib}"
   val recyclerview = "androidx.recyclerview:recyclerview:1.0.0-beta01"
   val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha1"
   val cardview = "androidx.cardview:cardview:1.0.0-beta01"
 
   val viewmodel = "android.arch.lifecycle:viewmodel:1.1.1"
   val livedata = "android.arch.lifecycle:livedata:1.1.1"
-  val gms = "com.google.android.gms:play-services-location:15.0.0"
-
-  val dagger = "com.google.dagger:dagger:2.16"
-  val dagger_compiler = "com.google.dagger:dagger-compiler:2.16"
-  val dagger_android = "com.google.dagger:dagger-android:2.16"
-  val dagger_android_support = "com.google.dagger:dagger-android-support:2.16"
-  val dagger_android_compiler = "com.google.dagger:dagger-android-processor:2.16"
 
   val junit = "junit:junit:4.12"
   val support_test = "com.android.support.test:runner:1.0.1"
